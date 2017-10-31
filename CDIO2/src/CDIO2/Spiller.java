cb727d20aa441a7267ac3febe28ad83066eff8de
Zeydel <mads_seydel@yahoo.dk>
2017-10-31 15:17:42 +0100

    Konto + Spiller

--- CDIO2/src/CDIO2/Spiller.java
+++ CDIO2/src/CDIO2/Spiller.java
@@ -1,5 +1,16 @@
 package CDIO2;
 
 public class Spiller {
+	
+	String Navn;
+	int Position;
+	Konto Konto;
+	
+	public Spiller(String N, int P, Konto Konto)
+	{
+		Navn = N;
+		Position = P;
+		this.Konto = Konto;
+	}
 
 }
--- CDIO2/src/CDIO2/spilSpil.java
+++ CDIO2/src/CDIO2/spilSpil.java
@@ -2,6 +2,7 @@ package CDIO2;
 
 public class spilSpil {
 	public static void main (String[] args) {
-	//test
+	
+		
 	}
 }
