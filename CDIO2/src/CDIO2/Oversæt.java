1aa95d6755669de222005a00f5c456ae93d35a99
Alex <Alex@192.168.0.18>
2017-11-02 17:01:35 +0100

    med oversættelse og næsten færdigt bræt, der er dog nogle fejl

--- CDIO2/src/CDIO2/Oversæt.java
+++ CDIO2/src/CDIO2/Oversæt.java
@@ -1,5 +1,89 @@
 package CDIO2;
 
-public class Oversæt {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import desktop_resources.GUI;
+
+public class Oversæt {
+	String line = "\r\n";
+
+	public static ArrayList<String> file1() {
+
+		ArrayList<String> lines1 = new ArrayList<String>();
+
+		try {
+			BufferedReader reader1 = new BufferedReader(new FileReader("file1.txt"));
+			String line = "\r\n";
+			while ((line = reader1.readLine()) != null) {
+				lines1.add(line);
+			}
+			System.out.println(lines1);
+			reader1.close();
+		} catch (IOException e) {
+			System.out.println("error");
+		} finally {
+			System.out.println("board created");
+		}
+		return lines1;
+	}
+
+	public static ArrayList<String> file2() {
+
+		ArrayList<String> lines2 = new ArrayList<String>();
+		try {
+			BufferedReader reader2 = new BufferedReader(new FileReader("file2.txt"));
+			String line = "\r\n";
+			while ((line = reader2.readLine()) != null) {
+				lines2.add(line);
+			}
+			System.out.println(lines2);
+			reader2.close();
+		} catch (IOException e) {
+			System.out.println("error");
+		} finally {
+			System.out.println("board created");
+		}
+		return lines2;
+	}
+
+	public static ArrayList<String> file3() {
+
+		ArrayList<String> lines1 = new ArrayList<String>();
+		try {
+			BufferedReader reader1 = new BufferedReader(new FileReader("file3.txt"));
+			String line = "\r\n";
+			while ((line = reader1.readLine()) != null) {
+				lines1.add(line);
+			}
+			System.out.println(lines1);
+			reader1.close();
+		} catch (IOException e) {
+			System.out.println("error");
+		} finally {
+			System.out.println("board created");
+		}
+		return lines1;
+	}
+
+	public static ArrayList<String> file4() {
+
+		ArrayList<String> lines1 = new ArrayList<String>();
+		try {
+			BufferedReader reader1 = new BufferedReader(new FileReader("file4.txt"));
+			String line = "\r\n";
+			while ((line = reader1.readLine()) != null) {
+				lines1.add(line);
+			}
+			System.out.println(lines1);
+			reader1.close();
+		} catch (IOException e) {
+			System.out.println("error");
+		} finally {
+			System.out.println("board created");
+		}
+		return lines1;
+	}
 }
--- CDIO2/src/CDIO2/spilSpil.java
+++ CDIO2/src/CDIO2/spilSpil.java
@@ -3,6 +3,8 @@ package CDIO2;
 public class spilSpil {
 	public static void main (String[] args) {
 	
+		Felt.BoardController();
+	
 		
 	}
 }
