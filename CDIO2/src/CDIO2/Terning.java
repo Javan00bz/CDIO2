d287fa68d7ab40f4615c4bb8f20709bca274e3d8
Zeydel <mads_seydel@yahoo.dk>
2017-11-02 16:40:38 +0100

    Terning fix

--- CDIO2/src/CDIO2/Terning.java
+++ CDIO2/src/CDIO2/Terning.java
@@ -2,14 +2,23 @@ package CDIO2;
 
 public class Terning {
 	
+	static int antalØjne;
+	
+	public Terning(int øjne) {
+		øjne = antalØjne;
+	}
 	
 	public static int rulTerning() {
-		int Terning = (int)(Math.random()*6+1);
-		return Terning;
+		antalØjne = (int)(Math.random()*6+1);
+		return antalØjne;
 	}
 	
-
-	
-	
+	public static int getAntalØjne() {
+		return antalØjne;
 	}
-
+	
+	public static void setAntalØjne(int nytAntalØjne) {
+		antalØjne = nytAntalØjne;
+	}
+	
+}
