648eb374fbe80133ac24953cae053229fa850835
oliveruschultz <oliveruldallschultz@gmail.com>
2017-11-02 16:46:59 +0100

    tern

--- CDIO2/src/CDIO2/Terning.java
+++ CDIO2/src/CDIO2/Terning.java
@@ -1,7 +1,6 @@
 package CDIO2;
 
 public class Terning {
-<<<<<<< HEAD
 		 
 		static int antalØjne;
 		
@@ -21,26 +20,3 @@ public class Terning {
 		public static void setAntalØjne(int nytAntalØjne) {
 			antalØjne = nytAntalØjne;
 		}
-=======
-	
-	static int antalØjne;
-	
-	public Terning(int øjne) {
-		
-		øjne = antalØjne;
-	}
-	
-	public static int rulTerning() {
-		antalØjne = (int)(Math.random()*6+1);
-		return antalØjne;
-	}
-	
-	public static int getAntalØjne() {
-		return antalØjne;
-	}
-	
-	public static void setAntalØjne(int nytAntalØjne) {
-		antalØjne = nytAntalØjne;
->>>>>>> branch 'Development' of https://github.com/Javan00bz/CDIO2.git
-		
-	}
