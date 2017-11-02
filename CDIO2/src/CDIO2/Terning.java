a19b588192049c531bef28c17b88bad853446abc
Zeydel <mads_seydel@yahoo.dk>
2017-11-02 15:49:25 +0100

    Raflebæger + Terning + Nu er det på Dansk

--- CDIO2/src/CDIO2/Terning.java
+++ CDIO2/src/CDIO2/Terning.java
@@ -1,5 +1,15 @@
 package CDIO2;
 
 public class Terning {
+	
+	
+	public static int rulTerning() {
+		int Terning = (int)(Math.random()*6+1);
+		return Terning;
+	}
+	
 
-}
+	
+	
+	}
+
--- CDIO2/src/CDIO2/spilSpil.java
+++ CDIO2/src/CDIO2/spilSpil.java
@@ -1,7 +1,11 @@
 package CDIO2;
 
 public class spilSpil {
+	
 	public static void main (String[] args) {
+		
+	Raflebæger.rollCup(5);
+	Raflebæger.getSum();
 	
 		
 	}
